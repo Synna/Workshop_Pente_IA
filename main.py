81033caec326ab933c6cc2825f8458ac42ca34b9
Unknown <robin.cornec@epsi.fr>
2017-04-12 14:20:52 +0200

    stage

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, url_for
+from flask import request
+from flask import json
+
 app = Flask(__name__)
 #http://blog.luisrei.com/articles/flaskrest.html
 
 
 @app.route('/board', methods = ['PUT'])
 def api_root():
-    return 'Welcome'
+    if request.headers['Content-Type'] == 'application/json':
+
+        return '{"x":"12","y":"5"}'
+    else:
+        return 'Pas de JSON'
 
 
 if __name__ == '__main__':
