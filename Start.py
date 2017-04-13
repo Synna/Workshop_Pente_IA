c7f119e363a7ff62110a64a992c148cf220e6e41
Unknown <robin.cornec@epsi.fr>
2017-04-13 10:43:51 +0200

    ca marche pas

--- Start.py
+++ Start.py
@@ -1,208 +1,206 @@
-def MinMax(board,score,score_vs,player,round):
-	self.board = board
-	self.score = score
-	self.score_vs = score_vs
-	self.player = player
-	self.round = round
-
-	ia = IA(self)
-	makeMove = IA.makeMove(self)
-	print(makeMove)
-	return {"x":"6", "y":"7"}
-
-
 import os
 import time
 import copy
 
-
 AI_POSITION = True
 PLAYER_POSITION = False
 
+
+
 class Ai:
-	outcomes = []
+    outcomes = []
 
-	def __init__(self):
-		self.board = self.board
-		self.player = self.game.player
-		self.lookingAt = AI_POSITION
+    def __init__(self, board, score, score_vs, player, round):
+        self.board = board
+        self.score = score
+        self.score_vs = score_vs
+        self.player = player
+        self.round = round
+        self.lookingAt = AI_POSITION
 
-	def isPosMine(self, x,y):
-		if self.board[x][y] == self.player:
-		    return True
-		else:
-			return False
-		#if x<0 or y<0 or x>=len(self.board) or y>=len(self.board): return False
-		#if self.board[y][x] == self.lookingAt: return True
-		#return False		
-
-
-	def isPosTheirs(self,x,y):
-		if self.board[x][y] != self.player and self.board[x][y] != 0:
-		    return True
-		else:
-			return False
-		#if x<0 or y<0 or x>=len(self.board) or y>=len(self.board): return False	
-		#if self.board[y][x] == (not self.lookingAt): return True
-		#return False
+    def isPosMine(self, x,y):
+        if self.board[x][y] == self.player:
+            return True
+        else:
+            return False
+        #if x<0 or y<0 or x>=len(self.board) or y>=len(self.board): return False
+        #if self.board[y][x] == self.lookingAt: return True
+        #return False
 
 
-	def findConsecutiveVertical(self,x,y):
-		score = 0
-		for i in range(5):
-			y+=1
-			if self.isPosTheirs(x,y): return 0
-			if self.isPosMine(x,y): score+=1
-		return score
+    def isPosTheirs(self,x,y):
+        if self.board[x][y] != self.player and self.board[x][y] != 0:
+            return True
+        else:
+            return False
+        #if x<0 or y<0 or x>=len(self.board) or y>=len(self.board): return False
+        #if self.board[y][x] == (not self.lookingAt): return True
+        #return False
 
 
-	def findConsecutiveHorizontal(self,x,y):
-		score = 0
-		for i in range(5):
-			x+=1
-			if self.isPosTheirs(x,y): return 0
-			if self.isPosMine(x,y): score+=1
-		return score
+    def findConsecutiveVertical(self,x,y):
+        score = 0
+        for i in range(5):
+            y+=1
+            if self.isPosTheirs(x,y): return 0
+            if self.isPosMine(x,y): score+=1
+        return score
 
 
-	def findConsecutiveDiagonalRight(self,x,y):
-		score = 0
-		for i in range(5):
-			x+=1
-			y+=1
-			if self.isPosTheirs(x,y): return 0
-			if self.isPosMine(x,y): score+=1
-		return score
-	
-
-	def findConsecutiveDiagonalLeft(self,x,y):
-		score = 0
-		for i in range(5):
-			x-=1
-			y+=1
-			if self.isPosTheirs(x,y): return 0
-			if self.isPosMine(x,y): score+=1
-		return score
-				
-	positionFunctions = [findConsecutiveVertical,findConsecutiveHorizontal,findConsecutiveDiagonalRight,findConsecutiveDiagonalLeft]
-
-	def getConsecutive(self):
-		points = {}
-		for i in range(len(self.board)):
-			for j in range(len(self.board[i])):
-				for positionFunction in self.positionFunctions:
-					score = positionFunction(self,i,j)
-					if score>0:
-						if score not in points: points[score] = 1
-						else: points[score] += 1
-		return points
+    def findConsecutiveHorizontal(self,x,y):
+        score = 0
+        for i in range(5):
+            x+=1
+            if self.isPosTheirs(x,y): return 0
+            if self.isPosMine(x,y): score+=1
+        return score
 
 
-	def valueGameState(self,coords):
-		self.board = self.board
-		self.board[coords[0]][coords[1]] = True
-		consecutive = self.getConsecutive()
-		self.board[coords[0]][coords[1]] = None
-		return consecutive
+    def findConsecutiveDiagonalRight(self,x,y):
+        score = 0
+        for i in range(5):
+            x+=1
+            y+=1
+            if self.isPosTheirs(x,y): return 0
+            if self.isPosMine(x,y): score+=1
+        return score
 
 
-	def valueBoard(self,moveBoard):
-		self.board = moveBoard
-		return self.getConsecutive()
+    def findConsecutiveDiagonalLeft(self,x,y):
+        score = 0
+        for i in range(5):
+            x-=1
+            y+=1
+            if self.isPosTheirs(x,y): return 0
+            if self.isPosMine(x,y): score+=1
+        return score
+
+    positionFunctions = [findConsecutiveVertical,findConsecutiveHorizontal,findConsecutiveDiagonalRight,findConsecutiveDiagonalLeft]
+
+    def getConsecutive(self):
+        points = {}
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                for positionFunction in self.positionFunctions:
+                    score = positionFunction(self,i,j)
+                    if score>0:
+                        if score not in points: points[score] = 1
+                        else: points[score] += 1
+        return points
 
 
-	def generateMoves(self,board):
-		coords = set()
-		for i in range(len(board)):
-			for j in range(len(board)):
-				if board[i][j] != None:
-					cols = range(max(i-2,0),min(i+3,len(board)))
-					rows = range(max(j-2,0),min(j+3,len(board)))
-					points_to_add = {(p_i,p_j) for p_i in rows for p_j in cols if (p_i,p_j) not in coords and board[p_i][p_j]==None}
-					coords.update(points_to_add)
-		return coords
+    def valueGameState(self,coords):
+        self.board = self.board
+        self.board[coords[0]][coords[1]] = True
+        consecutive = self.getConsecutive()
+        self.board[coords[0]][coords[1]] = None
+        return consecutive
 
 
-	def compareScores(self,score,highScore):
-		return score>highScore
+    def valueBoard(self,moveBoard):
+        self.board = moveBoard
+        return self.getConsecutive()
 
 
-	def getScore(self,scores):
-		score = 0
-		for number,points in scores.items(): 
-			score+= 4**number*points
-		return score
+    def generateMoves(self,board):
+        coords = set()
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] != None:
+                    cols = range(max(i-2,0),min(i+3,len(board)))
+                    rows = range(max(j-2,0),min(j+3,len(board)))
+                    points_to_add = {(p_i,p_j) for p_i in rows for p_j in cols if (p_i,p_j) not in coords and board[p_i][p_j]==None}
+                    coords.update(points_to_add)
+        return coords
 
 
-	def getBestMove(self,board,movingYou):
-		moves = self.generateMoves(board)
-		bestMove = (None,-float("inf"))
-		for move in moves:
-			self.lookingAt=movingYou==AI_POSITION
-			score = self.getScore(self.valueGameState(move))
-			self.lookingAt = movingYou==PLAYER_POSITION
-			score -= 1.5*self.getScore(self.valueGameState(move))
-			if self.compareScores(score,bestMove[1]):
-				bestMove = (move,score)
-		return bestMove
+    def compareScores(self,score,highScore):
+        return score>highScore
 
 
-	def get_best_move_from_branch(self,branch,person_moving = AI_POSITION):
-		best_score = float("inf")
-		best_move = None
-		if person_moving:
-			best_score *= -1
-		if isinstance(branch,dict):
-			for score in branch.values():
-				move_score = self.get_best_move_from_branch(score,person_moving = (not person_moving))
-				if person_moving:
-					if best_score < move_score and person_moving:
-						best_score = move_score
-				else:
-					if best_score > move_score and not person_moving:
-						best_score = move_score
-		else:
-			best_score = branch
-		return best_score
-						
-					
-
-	def get_best_move_from_tree(self,tree,person_moving = AI_POSITION):
-		best_move = None
-		best_score = float("inf")
-		if person_moving:
-			best_score *= -1
-		for move, branch in tree.items():
-			score = self.get_best_move_from_branch(branch,person_moving = (not person_moving))
-			if person_moving:
-				if score >= best_score and person_moving:
-					best_score = score
-					best_move = move
-			else:
-				if score <= best_move and not person_moving:
-					best_score = score
-					best_move = move
-		print(best_move)
-		print(best_score)
-		return best_move
+    def getScore(self,scores):
+        score = 0
+        for number,points in scores.items():
+            score+= 4**number*points
+        return score
 
 
-	def thinkDownTree(self, board, stepsRemaining,personMoving=AI_POSITION):
-		if stepsRemaining >0:
-			tree = {}
-			moves = self.generateMoves(board)
-			for move in moves:
-				movedBoard = board
-				movedBoard[move[0]][move[1]] = personMoving
-				tree[move] = self.thinkDownTree(movedBoard,stepsRemaining=stepsRemaining-1,personMoving=(not personMoving))
-				movedBoard[move[0]][move[1]] = None
-			return tree
-		else:
-			return self.getScore(self.valueBoard(board))
+    def getBestMove(self,board,movingYou):
+        moves = self.generateMoves(board)
+        bestMove = (None,-float("inf"))
+        for move in moves:
+            self.lookingAt=movingYou==AI_POSITION
+            score = self.getScore(self.valueGameState(move))
+            self.lookingAt = movingYou==PLAYER_POSITION
+            score -= 1.5*self.getScore(self.valueGameState(move))
+            if self.compareScores(score,bestMove[1]):
+                bestMove = (move,score)
+        return bestMove
 
 
-	def makeMove(self):
-		tree = self.thinkDownTree(self.board,stepsRemaining=2)
-		print({move: self.get_best_move_from_branch(branch) for move, branch in tree.items()})
-		x = self.get_best_move_from_tree(tree)
-		return x 
+    def get_best_move_from_branch(self,branch,person_moving = AI_POSITION):
+        best_score = float("inf")
+        best_move = None
+        if person_moving:
+            best_score *= -1
+        if isinstance(branch,dict):
+            for score in branch.values():
+                move_score = self.get_best_move_from_branch(score,person_moving = (not person_moving))
+                if person_moving:
+                    if best_score < move_score and person_moving:
+                        best_score = move_score
+                else:
+                    if best_score > move_score and not person_moving:
+                        best_score = move_score
+        else:
+            best_score = branch
+        return best_score
+
+
+
+    def get_best_move_from_tree(self,tree,person_moving = AI_POSITION):
+        best_move = None
+        best_score = float("inf")
+        if person_moving:
+            best_score *= -1
+        for move, branch in tree.items():
+            score = self.get_best_move_from_branch(branch,person_moving = (not person_moving))
+            if person_moving:
+                if score >= best_score and person_moving:
+                    best_score = score
+                    best_move = move
+            else:
+                if score <= best_move and not person_moving:
+                    best_score = score
+                    best_move = move
+        print(best_move)
+        print(best_score)
+        return best_move
+
+
+    def thinkDownTree(self, board, stepsRemaining,personMoving=AI_POSITION):
+        if stepsRemaining >0:
+            tree = {}
+            moves = self.generateMoves(board)
+            for move in moves:
+                movedBoard = board
+                movedBoard[move[0]][move[1]] = personMoving
+                tree[move] = self.thinkDownTree(movedBoard,stepsRemaining=stepsRemaining-1,personMoving=(not personMoving))
+                movedBoard[move[0]][move[1]] = None
+            return tree
+        else:
+            return self.getScore(self.valueBoard(board))
+
+
+    def makeMove(self):
+        tree = self.thinkDownTree(self.board,stepsRemaining=2)
+        print({move: self.get_best_move_from_branch(branch) for move, branch in tree.items()})
+        x = self.get_best_move_from_tree(tree)
+        return xl
+
+    def MinMax(self):
+
+        move_make = self.makeMove()
+        print(move_make)
+
+        return {"x": "6", "y": "7"}
--- main.py
+++ main.py
@@ -1,30 +1,36 @@
-from flask import Flask,request, url_for
+from flask import Flask, request, url_for
 from flask import Response
 from flask import json
 import Start
 
 app = Flask(__name__)
 #http://blog.luisrei.com/articles/flaskrest.html
+# {
+# "board":[["0","1","0"],["0","0","2"],["0","0","0"]],
+# "score":"3",
+# "score_vs":"2",
+# "player":"1",
+# "round":"3"
+# }
 
 
 @app.route('/board', methods = ['PUT'])
-def api_root():
-    board = json.dumps(request.json['board']);
-    return board
-    #score = json.dumps(request.json['score']);
-    #score_vs = json.dumps(request.json['score_vs']);
-    #player = json.dumps(request.json['player']);
-    #round = json.dumps(request.json['round']);
-#
-    #valueToReturn = Start.MinMax(board,score,score_vs,player,round)
-    #print(valueToReturn)
-#
-    #js = json.dumps(valueToReturn)
-#
-    #resp = Response(js, status=200, mimetype='application/json')
-#
-    #return resp
+def api_board():
+    board = json.dumps(request.json['board'])
+    score = json.dumps(request.json['score']);
+    score_vs = json.dumps(request.json['score_vs']);
+    player = json.dumps(request.json['player']);
+    round = json.dumps(request.json['round']);
 
+    launch_ia = Start.Ai(board, score, score_vs, player, round)
+    valueToReturn = launch_ia.MinMax()
+    print(valueToReturn)
+
+    js = json.dumps(valueToReturn)
+
+    resp = Response(js, status=200, mimetype='application/json')
+
+    return resp
 
 if __name__ == '__main__':
     app.run()
