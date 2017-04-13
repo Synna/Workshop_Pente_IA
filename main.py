e41f8f72e354c8eb8f5ff34051bd45f2f90b0012
Antoine Garrec <antoine.garrec@epsi.fr>
2017-04-13 09:48:51 +0200

    Nenufar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask,request,json,Response
-import sco_functions
+from flask import Flask,request, url_for
+from flask import Response
+from flask import json
+import Start
 
 app = Flask(__name__)
 #http://blog.luisrei.com/articles/flaskrest.html
@@ -8,18 +10,20 @@ app = Flask(__name__)
 @app.route('/board', methods = ['PUT'])
 def api_root():
     board = json.dumps(request.json['board']);
-    score = json.dumps(request.json['score']);
-    score_vs = json.dumps(request.json['score_vs']);
-    player = json.dumps(request.json['player']);
-    round = json.dumps(request.json['round']);
-
-    valueToReturn = MinMax(board,score,score_vs,player,round)
-
-    js = json.dumps(valueToReturn)
-
-    resp = Response(js, status=200, mimetype='application/json')
-
-    return resp
+    return board
+    #score = json.dumps(request.json['score']);
+    #score_vs = json.dumps(request.json['score_vs']);
+    #player = json.dumps(request.json['player']);
+    #round = json.dumps(request.json['round']);
+#
+    #valueToReturn = Start.MinMax(board,score,score_vs,player,round)
+    #print(valueToReturn)
+#
+    #js = json.dumps(valueToReturn)
+#
+    #resp = Response(js, status=200, mimetype='application/json')
+#
+    #return resp
 
 
 if __name__ == '__main__':
